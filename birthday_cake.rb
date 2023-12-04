8daaad2c0d5f59c29a7261a1ccd3d3da659593fe
Mary Webby <118230483+marywebby@users.noreply.github.com>
2023-12-04 23:10:16 +0000

    fixed bug issues, program runs perfect

--- birthday_cake.rb
+++ birthday_cake.rb
@@ -32,13 +32,23 @@ class BirthdayCake
   def candles_status
     if self.lit == true
       return "lit"
-    else
+    elsif self.lit == false
       return "blown out"
+    else 
+      return "no candles"
     end
   end
 
+  def light_candles
+    self.lit = true
+  end
+
+  def blow_out_candles
+    self.lit = false
+  end
+
   def greet
-    "Happy #{self.age.ordinalize} Birdday!"
+    "Happy #{self.age.ordinalize} Birthday!"
   end
 
   def sing
@@ -50,16 +60,16 @@ class BirthdayCake
   end
 
   def BirthdayCake.celebrate(age)
-    birthday_cake = Birthdaycake.new(age)
+    birthday_cake = BirthdayCake.new(age)
     puts birthday_cake.greet
     puts birthday_cake.to_s
     puts "Lighting candles..."
     birthday_cake.light_candles
     puts birthday_cake.to_s
     puts "Singing happy birthday..."
-    puts birthday_cake.sign
+    puts birthday_cake.sing
     puts "Blowing out candles..."
-    birth_cake.blow_out_candles
+    birthday_cake.blow_out_candles
     puts birthday_cake.to_s
 
     birthday_cake
